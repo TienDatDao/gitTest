30be40f4645dabb6464c27e79fa4c4e47ef3acfa
hieuct26py <hieuct26.py@gmail.com>
2024-10-01 14:14:52 +0700

    finish first

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define MX 100005
+#define pathio "D:/code/put/"
+#define lfi pathio "input.txt", "r", stdin
+#define lfo pathio "output.txt", "w", stdout
+
+using namespace std;
+//void faster() {
+    //ios_base::sync_with_stdio(0);
+    //cin.tie(0);
+    //cout.tie(0);
+// }
+
+
+signed main() {
+    freopen(lfi);
+    freopen(lfo);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    cout << n;
+
+    fclose(stdin);
+    fclose(stdout);
+    return 0;
+}
